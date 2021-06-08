acbe15ce630620566a364e075dc4719f376a7f7f
wooihaw <wooihaw.tan@gmail.com>
2021-06-08 13:57:10 +0800

    gitautosync update 36

--- day_1.py
+++ day_1.py
@@ -78,3 +78,5 @@ print(-(2 ** 2))  # this will give us -4
 print((-2) ** 2)  # this will give us 4
 
 #%% Input, output, loops and conditional statement
+ans = input('Enter a number: ')
+print(f'{ans=}, type(ans)')
