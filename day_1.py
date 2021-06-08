381373d4c56b72202d390111c76b1649327a2bf5
wooihaw <wooihaw.tan@gmail.com>
2021-06-08 14:10:33 +0800

    gitautosync update 43

--- day_1.py
+++ day_1.py
@@ -89,3 +89,10 @@ i = 0
 while i < 10:
     print(i)
     i += 1  # i = i + 1
+    
+while True:
+    ans = input('Do you want to continue? (y/n) ')
+    if ans == 'n':
+        break
+    elif ans == 'y':
+        print("Let's continue")
