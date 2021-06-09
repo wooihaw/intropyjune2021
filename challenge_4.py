6c3d6155224ecfd46d43aefd9f1ce1958881aa14
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:50:45 +0800

    gitautosync update 81

--- challenge_4.py
+++ challenge_4.py
@@ -5,4 +5,7 @@ Created on Wed Jun  9 14:47:23 2021
 @author: wooihaw
 """
 # Challenge 4
-s1 = set(range(1, 101))
+s1 = set(range(1, 101))  # number from 1 to 100
+s2 = set(range(5, 101, 5)) # numbers divisible by 5
+s3 = set(range(7, 101, 7)) # number divisible by 7
+
