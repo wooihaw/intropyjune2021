ae4a7ff5d53417f5e17e9d20a6023c25c58af662
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:53:37 +0800

    gitautosync update 83

--- challenge_4.py
+++ challenge_4.py
@@ -9,5 +9,5 @@ s1 = set(range(1, 101))  # number from 1 to 100
 s2 = set(range(5, 101, 5)) # numbers divisible by 5
 s3 = set(range(7, 101, 7)) # number divisible by 7
 
-s = s1 - (s2 | s3)
+s = s1 - (s2 | s3)  # numbers that are not divisible by 5 or 7
 print(f'{s=}')
