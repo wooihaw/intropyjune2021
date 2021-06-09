47b18563200b55c0cc84180af004f357c485593d
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:58:41 +0800

    gitautosync update 85

--- challenge_5.py
+++ challenge_5.py
@@ -5,3 +5,11 @@ Created on Wed Jun  9 14:55:20 2021
 @author: wooihaw
 """
 # Challenge 5
+d = {}
+for i in range(3):
+    name = input(f'Enter name {i+1}: ')
+    age = input(f'Enter age {i+1}: ')
+    d[name] = age
+
+n = input('Enter a name to search: ')
+print(f'{n}\'s age is {d[n]}')
