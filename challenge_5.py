5e0fa7a2fb717804ad4176181e0466e0d2c71ce4
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 15:02:33 +0800

    gitautosync update 87

--- challenge_5.py
+++ challenge_5.py
@@ -9,7 +9,7 @@ d = {}
 for i in range(3):
     name = input(f'Enter name {i+1}: ')
     age = input(f'Enter age {i+1}: ')
-    d[name] = age
+    d[name] = age  # store name & age in dictionary as key-value pair
 
 n = input('Enter a name to search: ')
 print(f'{n}\'s age is {d.get(n, "unknown")}')
