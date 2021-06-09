a7277ba13f20c4c5839c13cf2f1ef1512cbb5dfa
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:18:57 +0800

    gitautosync update 70

--- challenge_1.py
+++ challenge_1.py
@@ -5,3 +5,10 @@ Created on Wed Jun  9 14:15:26 2021
 @author: wooihaw
 """
 # Challenge 1
+heads = 35
+legs = 94
+
+for r in range(heads+1):
+    c = heads - r
+    if (2*c + 4*r) == legs:
+        print(f'We have {r} rabbits and {c} chicken')
