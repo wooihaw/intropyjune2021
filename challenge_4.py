c48039f3101ad915345ae97888ade37118d8ee29
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:52:52 +0800

    gitautosync update 82

--- challenge_4.py
+++ challenge_4.py
@@ -9,3 +9,5 @@ s1 = set(range(1, 101))  # number from 1 to 100
 s2 = set(range(5, 101, 5)) # numbers divisible by 5
 s3 = set(range(7, 101, 7)) # number divisible by 7
 
+s = s1 - (s2 | s3)
+print(f'{s=}')
