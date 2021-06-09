8d52389c9f3c9ccd5dd3cf39d728f49e51fce16c
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 15:10:30 +0800

    gitautosync update 89

--- challenge_6.py
+++ challenge_6.py
@@ -9,3 +9,7 @@ s = '''Peter Piper picked a peck of pickled peppers.
 A peck of pickled peppers Peter Piper picked.
 If Peter Piper picked a peck of pickled peppers,
 where is the peck of pickled peppers Peter Piper picked?'''
+
+t = [c if c.isalpha() else ' ' for c in s]
+s2 = ''.join(t)
+print(f'{t=}\n{s2=}')
