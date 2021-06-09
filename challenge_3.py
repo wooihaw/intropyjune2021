7824be2174b34b87d2643016b40d7b52ec725b7a
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:39:42 +0800

    gitautosync update 78

--- challenge_3.py
+++ challenge_3.py
@@ -6,3 +6,10 @@ Created on Wed Jun  9 14:37:06 2021
 """
 # Challenge 3
 alist = ['Python', 'Java', 'Perl', 'PHP', 'JavaScript', 'C++', 'C#', 'Ruby', 'R']
+
+# Method 1 - Using for loop
+blist = []
+for i in alist:
+    if i[0].upper()=='P':
+        blist.append(i)
+print(f'{blist=}')
