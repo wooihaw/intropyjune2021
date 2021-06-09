872bce7ef7d51098dff44018083403f3812cc0ee
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 15:06:42 +0800

    gitautosync update 88

---
+++ challenge_6.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  9 15:05:41 2021
+
+@author: wooihaw
+"""
+# Challenge 6
+s = '''Peter Piper picked a peck of pickled peppers.
+A peck of pickled peppers Peter Piper picked.
+If Peter Piper picked a peck of pickled peppers,
+where is the peck of pickled peppers Peter Piper picked?'''
