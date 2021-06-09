e9f239b411bb7ce2fe5971c359851798e9487a74
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 09:47:35 +0800

    gitautosync update 4

--- day_2.py
+++ day_2.py
@@ -7,3 +7,5 @@ Created on Wed Jun  9 09:45:26 2021
 #%% Basic list examples
 alist = [1, 2.3, 4+5j, 'abc', [6, 7, -8.9]]
 print(alist[0])  # first item
+print(alist[-1])  # last item
+print(alist[-1][-1])  # this will print out -8.9
