e023c0fb82df09b546ebbc0f7fe158e789423489
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:35:24 +0800

    gitautosync update 75

--- challenge_2.py
+++ challenge_2.py
@@ -9,7 +9,7 @@ investment = eval(input('Enter the amount of investment: '))
 interest = eval(input('Enter the interest rate (%): '))
 duration = eval(input('Enter the duration of investment (years): '))
 
-print(f'Initial investment: RM{investment}, annual rate: {interest}%, years of investment: {duration}')
+print(f'Initial investment: RM{investment:.2f}, annual rate: {interest}%, years of investment: {duration}')
 
 for i in range(duration):
     investment = investment + investment * interest/100
