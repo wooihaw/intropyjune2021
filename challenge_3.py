d69903576c3e510c0c1c54f1526721d93a263023
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:37:45 +0800

    gitautosync update 77

---
+++ challenge_3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  9 14:37:06 2021
+
+@author: wooihaw
+"""
+# Challenge 3
+alist = ['Python', 'Java', 'Perl', 'PHP', 'JavaScript', 'C++', 'C#', 'Ruby', 'R']
