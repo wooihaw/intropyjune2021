891bcf5a8f2f5de3025621c9da554c51336663c3
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 14:25:42 +0800

    gitautosync update 72

--- challenge_2.py
+++ challenge_2.py
@@ -5,3 +5,6 @@ Created on Wed Jun  9 14:23:29 2021
 @author: wooihaw
 """
 # Challenge 2
+investment = input('Enter the amount of investment: ')
+interest = input('Enter the interest rate (%): ')
+duration = input('Enter the duration of investment (years): ')
