cffecc9c50cd07e944457745b8d86eba62aac075
wooihaw <wooihaw.tan@gmail.com>
2021-06-09 15:23:46 +0800

    gitautosync update 94

--- challenge_6.py
+++ challenge_6.py
@@ -17,3 +17,17 @@ u = sorted(set(w))
 print(f'{t=}\n{s2=}\n{w=}\n{u=}')
 
 wc = {}
+for i in u:
+    wc[i] = w.count(i)
+print(f'{wc=}')
+
+# Method 2
+wc2 = {}
+for i in sorted(w):
+    wc2[i] = wc2.get(i, 0) + 1
+print(f'{wc2=}')
+
+# Method 3
+from collections import Counter
+wc3 = Counter(w)
+print(f'{wc3=}')
