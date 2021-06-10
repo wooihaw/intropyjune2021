dd766daf3e08dde130242d61f5b87c66d55b442e
wooihaw <wooihaw.tan@gmail.com>
2021-06-10 16:16:42 +0800

    gitautosync update 64

--- challenge_10.py
+++ challenge_10.py
@@ -5,9 +5,15 @@ Created on Thu Jun 10 16:11:19 2021
 @author: wooihaw
 """
 # Challenge 10
+import numpy as np
+
 temperatures = []
 with open('data/Heathrow.txt') as f:
     for row in f:
         temperatures.append(float(f.readline().strip()))
 
 print(temperatures)
+
+print(f'{np.min(temperatures)=}', f'{np.max(temperatures)=}',
+      f'{np.mean(temperatures)=}', f'{np.median(temperatures)=}',
+      sep = '\n')
