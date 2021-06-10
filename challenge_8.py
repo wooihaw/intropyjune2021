f90119a33b296ecf655e6fc8ca8fd63b339d41b4
wooihaw <wooihaw.tan@gmail.com>
2021-06-10 15:59:23 +0800

    gitautosync update 58

--- challenge_8.py
+++ challenge_8.py
@@ -17,9 +17,10 @@ class Circle:
         return 2 * self.__pi * self.r
     
     def __str__(self):
-        return f'A circle with the radius os {self.r}'
+        return f'A circle with the radius of {self.r}'
 
 # This part will run if the script is executed as a Python program
 if __name__ == '__main__':
     c1 = Circle(4)
     print(f'Area={c1.area()}, circumference={c1.circumference()}')
+    print(c1)
