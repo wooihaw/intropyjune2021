0d8efbffe3cdb98362566c052cdce719c656acc7
wooihaw <wooihaw.tan@gmail.com>
2021-06-10 16:29:19 +0800

    gitautosync update 67

--- challenge_11.py
+++ challenge_11.py
@@ -7,8 +7,10 @@ Created on Thu Jun 10 16:21:35 2021
 # Challenge 11
 
 from collections import Counter
+import easygui as gui
 
-with open('data/alice.txt', 'r') as f:
+filename = gui.fileopenbox(title='Open file', default='*.txt')
+with open(filename, 'r') as f:
     s = f.read()
 
 t = [c.lower() if c.isalpha() else ' ' for c in s]
@@ -16,3 +18,5 @@ w = (''.join(t)).split()
 
 wc = Counter(w)
 print(f'Number of unique words: {len(wc)}')
+print(f'{wc.most_common(10)}')
+print(f'The word "alice" appears {wc["alice"]} times in the text.')
