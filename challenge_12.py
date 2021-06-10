3e78f4b6e5adb82375bd610df5967dc0c7a5ac72
wooihaw <wooihaw.tan@gmail.com>
2021-06-10 16:37:33 +0800

    gitautosync update 70

--- challenge_12.py
+++ challenge_12.py
@@ -9,4 +9,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/covid19.csv')
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+
 print(df)
+
+cases_5000 = df['Case'] > 5000
+print(f'There are {sum(cases_5000)} days with more than 5000 cases')
+print(df[cases_5000])
