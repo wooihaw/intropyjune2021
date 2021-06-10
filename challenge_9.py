5dc6b88a3abf0a0a0ebb1b7e8e6b400925f578a1
wooihaw <wooihaw.tan@gmail.com>
2021-06-10 16:02:30 +0800

    gitautosync update 59

---
+++ challenge_9.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 16:00:59 2021
+
+@author: wooihaw
+"""
+# Challenge 9
+from random import choice, shuffle
+animals = ['wolf', 'whale', 'cheetah', 'lizard', 'tiger', 'monkey', 'parrot',
+'gorilla', 'dolphin', 'snake']
+
+word = choice(animals)
+print(word)
