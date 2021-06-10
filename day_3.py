687ba78f13735d32f1116b43b77df131d8ad10b4
wooihaw <wooihaw.tan@gmail.com>
2021-06-10 09:13:22 +0800

    gitautosync update 2

--- day_3.py
+++ day_3.py
@@ -5,3 +5,13 @@ Created on Thu Jun 10 09:06:56 2021
 @author: wooihaw
 """
 #%% Day-2 Recap
+astring1 = 'This is a string'
+astring2 = "This is also a string"
+astring3 = "Let's start Pythong programming"
+
+alist = [1, 2.3, 4+5j, 'abc', [67.8, -9.1], {'apple':1.2, 'orange':2.5}]
+atuple = (1, 2.3, 4+5j, 'abc')
+adict = {'name': 'John', 'phone': 12345678}
+aset = {1, 2.3, 'abc'}
+
+print(f'{astring1.upper()}')
